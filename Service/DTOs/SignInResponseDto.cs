646e44047f89a182a9ba8c26eadf902d88ffb9a4
Dan Maklen <danmaklen@gmail.com>
2022-10-01 22:06:06 +0200

    Fix DtoJsonPropertyNames

--- Service/DTOs/SignInResponseDto.cs
+++ Service/DTOs/SignInResponseDto.cs
@@ -1,6 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace Ana.Service.DTOs;
 
 public class SignInResponseDto
 {
+    [JsonPropertyName("token")]
     public string Token { get; init; } = default!;
 }
