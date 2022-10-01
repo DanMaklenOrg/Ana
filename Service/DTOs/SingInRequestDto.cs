ddaf256b1d3781abe11365c340589280231f685c
Omar Nasr <danmaklen@gmail.com>
2022-10-01 22:12:03 +0200

    Fix DtoJsonPropertyNames (#8)

--- Service/DTOs/SingInRequestDto.cs
+++ Service/DTOs/SingInRequestDto.cs
@@ -1,8 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Ana.Service.DTOs;
 
 public class SingInRequestDto
 {
+    [JsonPropertyName("username")]
     public string Username { get; set; } = default!;
 
+    [JsonPropertyName("password")]
     public string Password { get; set; } = default!;
 }
